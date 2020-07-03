c59949e8dc33f2cab6730e1ed866170d90b36f3d
mysterico <ian@mystericoui-iMac.local>
2020-07-03 11:20:16 +0900

    edit readme

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ Scrapy + pyQt5 를 이용하여 크롤링구현
 
 init.py로 시작하며 상품내용을 가진 현재시간.txt 및 상품사진(개별사진, 전체사진)을 가진 상품ID폴더가 생성
 
+<div>
+<image width="200" src="https://user-images.githubusercontent.com/58951614/86424648-1065d880-bd1e-11ea-8c5d-2b6532164b78.png">
+<image width="300" src="https://user-images.githubusercontent.com/58951614/86424849-95e98880-bd1e-11ea-985f-903355dbd12c.png">
+<image width="300" src="https://user-images.githubusercontent.com/58951614/86424957-e234c880-bd1e-11ea-9ef4-3e7c69376622.png">
+<image width="300" src="https://user-images.githubusercontent.com/58951614/86424993-f082e480-bd1e-11ea-975a-ff12ab9542db.png">
+</div>
+
+---
+
 ##### TODO
 
 1. requirements.txt
@@ -15,3 +24,5 @@ init.py로 시작하며 상품내용을 가진 현재시간.txt 및 상품사진
 프록시 작업을 하면 위의 제한을 피할 수 있다.
 
 constants.py의 COUPANG_SEARCH_CATEGORIES 값이 사이트 업데이트에 따라 변경될 수 있음
+
+#### Screenshot
---
+++ constants.py
@@ -0,0 +1,35 @@
+import os
+
+COUPANG_ID = ""
+COUPANG_PW = ""
+COUPANG_SEARCH_WORD = ""
+COUPANG_SEARCH_COUNT = 5
+COUPANG_SEARCH_CATEGORYID = None
+COUPANG_RESULT_DIR = os.getcwd()
+COUPANG_SEARCH_CATEGORIES = [
+    {"id": None, "name": "전체"},
+    {"id": 186664, "name": "여성패션"},
+    {"id": 186969, "name": "남성패션"},
+    {"id": 213101, "name": "베이비패션 (0~3세)"},
+    {"id": 213414, "name": "여아패션 (3세 이상)"},
+    {"id": 213641, "name": "남아패션 (3세 이상)"},
+    {"id": 187721, "name": "스포츠패션"},
+    {"id": 187265, "name": "신발"},
+    {"id": 187377, "name": "가방/잡화"},
+    {"id": 176422, "name": "뷰티"},
+    {"id": 221834, "name": "출산/유아동"},
+    {"id": 194176, "name": "식품"},
+    {"id": 185569, "name": "주방용품"},
+    {"id": 115573, "name": "생활용품"},
+    {"id": 184455, "name": "홈인테리어"},
+    {"id": 178155, "name": "가전디지털"},
+    {"id": 317678, "name": "스포츠/레저"},
+    {"id": 183960, "name": "자동차용품"},
+    {"id": 317677, "name": "도서/음반/DVD"},
+    {"id": 317679, "name": "완구/취미"},
+    {"id": 177195, "name": "문구/오피스"},
+    {"id": 115574, "name": "반려동물용품"},
+    {"id": 305698, "name": "헬스/건강식품"},
+    {"id": 396363, "name": "국내여행"},
+    {"id": 396364, "name": "해외여행"}
+]
